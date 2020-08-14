0e21e054906a059c11b22b18631afa9ba0946e28
Matteo Pagani <matteo.pagani@microsoft.com>
2020-08-14 16:59:28 +0200

    Add project files.

---
+++ OfficeInterop/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OfficeInterop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDoSomething(object sender, RoutedEventArgs e)
+        {
+            var application = new Microsoft.Office.Interop.Word.Application();
+            application.Visible = true;
+            var doc = application.Documents.Add();
+            doc.Content.Text = "Hello world!";
+        }
+    }
+}
